fc534c2f4b064a4f2dc57f62d50a906a2f30bdcf
mikeplavsky <Mike.Plavsky@quest.com>
2011-07-29 17:59:11 +0400

    Chat implementation

---
+++ chat.js
@@ -0,0 +1,26 @@
+var tcp = require('net');
+var sockets = [];
+
+var srv = tcp.createServer(function(socket) {
+  
+  sockets.push(socket);
+
+  socket.on( 'data', function (data) { 
+
+    for (var i = 0; i < sockets.length; ++i){
+      sockets[i].write( data );
+    };
+
+  }); 
+
+  socket.on( 'end', function () {
+
+    var i = sockets.indexOf(socket); 
+    sockets.splice( i, 1 ); 
+  
+  });
+
+});
+
+srv.listen(8000);
+
